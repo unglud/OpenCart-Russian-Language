cea5e753c6349fd894b0afe518f3c640a8b4604b
Alexander Matrosov <unglud@gmail.com>
2011-10-05 16:42:38 +0400

    catalog add

---
+++ catalog/language/russian/common/maintenance.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title']     = 'Maintenance';
+
+// Text
+$_['text_maintenance']  = 'Maintenance';
+$_['text_message']      = '<h1 style="text-align:center;">На сайте проводятся технические работы.<br/>Мы возобновим торговлю буквально через несколько минут.</h1>';
+?>
