9d759982c7b8bdbbcd4935678cc650f9d96ab076
Alexander Matrosov <unglud@gmail.com>
2012-01-11 15:46:19 +0400

    Новый релиз 1.5.2

--- trunk/catalog/language/russian/module/featured.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading 
-$_['heading_title']  = 'Рекомендуем';
-
-// Text
-$_['text_stars']     = '%s из 5 баллов!';
-?>
--- trunk/catalog/language/russian/module/latest.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading 
-$_['heading_title']  = 'Последние';
-
-// Text
-$_['text_stars']     = '%s из 5 баллов!';
-?>
