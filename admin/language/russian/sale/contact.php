2476c2c6c8068940575e3a714a467f92c9c8b99f
Alexander Matrosov <unglud@gmail.com>
2012-02-21 17:39:44 +0400

    Страница "почта" в админке

--- admin/language/russian/sale/contact.php
+++ admin/language/russian/sale/contact.php
@@ -4,15 +4,23 @@ $_['heading_title']    = 'Почта';
 
 // Text
 $_['text_success']     = 'Ваше сообщение отправлено!';
+$_['text_sent']        = 'Ваше сообщение отправлено %s из %s адресатов!';
 $_['text_default']     = 'По умолчанию';
 $_['text_newsletter']  = 'Подписчики на новости';
-$_['text_customer']    = 'Все покупатели';
-$_['text_search']      = 'Поиск';
+$_['text_customer_all']    = 'Все покупатели';
+$_['text_customer_group']  = 'Группе покупателей';
+$_['text_customer']    = 'Одному покупателю';
+$_['text_affiliate_all']   = 'Всем аффелиатам';
+$_['text_affiliate']       = 'Одному аффелиату';
+$_['text_product']         = 'По товарам';
 
 // Entry
 $_['entry_store']      = 'От:';
-$_['entry_product']    = 'Для клиентов, кто уже заказывал товары:';
 $_['entry_to']         = 'Кому:';
+$_['entry_customer_group'] = 'Группа покупателей:';
+$_['entry_customer']       = 'Покупатель:<br /><span class="help">Автозаполнение по имени</span>';
+$_['entry_affiliate']      = 'Аффелиат:<br /><span class="help">Автозаполнение по имени</span>';
+$_['entry_product']    = 'По товарам:<br /><span class="help">Отправить отлько тем, кто заказывал товары из списка (автозаполнение)</span>';
 $_['entry_subject']    = 'Тема:';
 $_['entry_message']    = 'Сообщение:';
 
