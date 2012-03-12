0119b8b3a0c31d1bd50ff8621863332d6d6c963e
Alexander Matrosov <unglud@gmail.com>
2012-03-12 22:53:40 +0400

    Исправлены 2 старых модуля

--- admin/language/russian/module/bestseller.php
+++ admin/language/russian/module/bestseller.php
@@ -1,21 +1,24 @@
 <?php
 // Heading
-$_['heading_title']    = 'Хиты продаж';
+$_['heading_title']    = 'Лидеры продаж';
 
 // Text
 $_['text_module']      = 'Модули';
 $_['text_success']     = 'Настройки модуля обновлены!';
-$_['text_left']        = 'Слева';
-$_['text_right']       = 'Справа';
+$_['text_content_top']    = 'Над контентом';
+$_['text_content_bottom'] = 'Под контентом';
+$_['text_column_left']    = 'Левая колонка';
+$_['text_column_right']   = 'Правая колонка';
 
 // Entry
+$_['entry_limit']      = 'Лимит:';
 $_['entry_image']         = 'Изображение (Ш x В):';
 $_['entry_layout']        = 'Шаблон:';
-$_['entry_limit']      = 'Лимит:';
 $_['entry_position']   = 'Позиция:';
 $_['entry_status']     = 'Статус:';
 $_['entry_sort_order'] = 'Порядок сортировки:';
 
 // Error
 $_['error_permission'] = 'У Вас нет прав для управления этим модулем!';
+$_['error_image']         = 'Размеры изображения обязательны!';
 ?>
