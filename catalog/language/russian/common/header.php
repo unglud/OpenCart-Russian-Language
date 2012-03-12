7ea84660465849ecbbba947b82578a8e5cf64fbe
Alexander Matrosov <unglud@gmail.com>
2012-03-12 20:47:49 +0400

    Небольшие изменения для 1.5.2.2

--- catalog/language/russian/common/header.php
+++ catalog/language/russian/common/header.php
@@ -2,8 +2,8 @@
 // Text
 $_['text_home']     = 'Главная';
 $_['text_wishlist'] = 'Отложенные (%s)';
-$_['text_cart']     = 'Корзина';
-$_['text_search']   = 'Search';
+$_['text_shopping_cart']     = 'Корзина';
+$_['text_search']   = 'Поиск';
 $_['text_welcome']  = 'Приветствуем! Вы можете <a href="%s">войти</a> или <a href="%s">зарегистрироваться</a>.';
 $_['text_logged']   = 'Вы вошли как <a href="%s">%s</a> <b>(</b> <a href="%s">Выйти</a> <b>)</b>';
 $_['text_account']  = 'Мой аккаунт';
