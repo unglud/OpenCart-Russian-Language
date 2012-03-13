24238adf8523626aeb4e958016fde98b3d9f4c93
Alexander Matrosov <unglud@gmail.com>
2012-03-13 17:44:05 +0400

    Исрправлен баг в фале информационных страниц админки

--- admin/language/russian/module/information.php
+++ admin/language/russian/module/information.php
@@ -5,10 +5,13 @@ $_['heading_title']    = 'Информация';
 // Text
 $_['text_module']      = 'Модули';
 $_['text_success']     = 'Настройки модуля обновлены!';
-$_['text_left']        = 'Слева';
-$_['text_right']       = 'Справа';
+$_['text_content_top']    = 'Над контентом';
+$_['text_content_bottom'] = 'Под контентом';
+$_['text_column_left']    = 'Левая колонка';
+$_['text_column_right']   = 'Правая колонка';
 
 // Entry
+$_['entry_layout']        = 'Шаблон:';
 $_['entry_position']   = 'Позиция:';
 $_['entry_status']     = 'Статус:';
 $_['entry_sort_order'] = 'Порядок сортировки:';
