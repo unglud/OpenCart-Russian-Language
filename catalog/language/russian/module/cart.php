6ae5e0e4880dcf95f9aa097016c37c85b37c1524
Alexander Matrosov <unglud@gmail.com>
2012-03-15 21:47:49 +0400

    #1 модуль корзины

---
+++ catalog/language/russian/module/cart.php
@@ -0,0 +1,10 @@
+<?php
+// Heading 
+$_['heading_title'] = 'В вашей корзине';
+
+// Text
+$['text_items'] = '%s шт. - %s';
+$['text_empty'] = 'Ваша корзина пуста!';
+$['text_cart'] = 'Открыть корзину';
+$['text_checkout'] = 'Оформить';
+?>
