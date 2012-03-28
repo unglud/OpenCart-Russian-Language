37f5645f34f51f84c3384332480d2309a22e64d8
Alexander Matrosov <unglud@gmail.com>
2012-03-28 16:22:31 +0400

    Много изменений в настройках сайта

--- admin/language/russian/setting/store.php
+++ admin/language/russian/setting/store.php
@@ -4,11 +4,16 @@ $_['heading_title']           = 'Управление магазинами';
 
 // Text
 $_['text_success']            = 'Магазины обновлены!';
+$_['text_items']                 = 'Items';
+$_['text_tax']                   = 'Taxes';
+$_['text_account']               = 'Account';
+$_['text_checkout']              = 'Checkout';
+$_['text_stock']                 = 'Stock';
 $_['text_image_manager']      = 'Менеджер изображений';
-$_['text_default']            = '&nbsp;<strong>(По умолчанию)</strong>';
-$_['text_edit_store']         = 'Редактировать магазин:';
 $_['text_browse']            = 'Обзор';
 $_['text_clear']             = 'Удалить';
+$_['text_shipping']            = 'Адрес доставки';
+$_['text_payment']             = 'Адрес оплаты';
 
 // Column
 $_['column_name']              = 'Название магазина';
@@ -16,60 +21,74 @@ $_['column_url']	           = 'URL магазина';
 $_['column_action']            = 'Действие';
 
 // Entry
+$_['entry_url']               = 'URL магазина:<br /><span class="help">Включая полный URL к магазину. Добавьте \'/\' в конце. Например: http://wwww.yourdomain.com/path/<br />Не используйте дериктории для новых магазинов. Необходимо использовать новый домен или поддомен.</span>';
+$_['entry_ssl']               = 'SSL URL:<br /><span class="help">SSL URL для вашего магазина. Добавьте \'/\' в конце. Например: http://wwww.yourdomain.com/path/<br />Не используйте дериктории для новых магазинов. Необходимо использовать новый домен или поддомен.</span>';
 $_['entry_name']              = 'Название магазина:';
-$_['entry_url']               = 'URL магазина:<br /><span class="help">Включая полный URL к магазину. Добавьте \'/\' в конце. Например: http://wwww.yourdomain.com/path/</span>';
-$_['entry_ssl']               = 'Использовать SSL:<br /><span class="help">Для использования SSL убедитесь что Ваш хостинг поддерживает SSL сертификат и пропишите SSL адреса в файле конфигурации.</span>';
+$_['entry_owner']              = 'Владелец магазина:';
+$_['entry_address']            = 'Адрес магазина:';
+$_['entry_email']              = 'Email:';
+$_['entry_telephone']          = 'Телефон:';
+$_['entry_fax']                = 'Факс:';
 $_['entry_title']             = 'Заголовок:';
 $_['entry_meta_description']  = 'Мета-теги:';
+$_['entry_layout']            = 'Шаблон по умолчанию:';
 $_['entry_template']          = 'Шаблон:';
-$_['entry_description']       = 'Сообщение приветствия:';
 $_['entry_country']           = 'Страна:';
 $_['entry_zone']              = 'Регион / Область:';
 $_['entry_language']          = 'Язык:';
 $_['entry_currency']          = 'Валюта:';
-$_['entry_tax']               = 'Отображать цены с налогом:';
+$_['entry_catalog_limit'] 	  = 'Элементов на страницу (Каталог):<br /><span class="help">Определяет количество элементов, показываемых на одной странице в каталоге (товары, категории и т.д.)</span>';$_['entry_tax']               = 'Отображать цены с налогом:';
+$_['entry_tax_default']        = 'Использовать налоговый адрес магазина:<br /><span class="help">Использовать адрес магазина для подсчета налогов если никто не авторизован. Можно выбрать адрес магазина для пользователей вместо их адреса.</span>';
+$_['entry_tax_customer']       = 'Использовать налоговый адрес пользователей:<br /><span class="help">Использовать пользовательский адрес при их авторизации для подсчета налогов.</span>';
 $_['entry_customer_group']    = 'Группа покупателей:<br /><span class="help">Группа покупателей по умолчанию.</span>';
 $_['entry_customer_price']    = 'Показывать цены:<br /><span class="help">Показывать цены только зарегистрированнным клиентам.</span>';
 $_['entry_customer_approval'] = 'Активация новых пользователей:<br /><span class="help">Не позволять входить новым клиентам под своим логином и паролем до одобрения аккаунта.</span>';
-$_['entry_guest_checkout']    = 'Гостевое оформление заказа:<br /><span class="help">Позволить клиентам оформлять заказы без регистрации Личного кабинета. Эта функция не будет доступна, если в корзине будут загружаемые в виде файла товары.</span>';
+$_['entry_company_no_display']   = 'Display Company No.:<br /><span class="help">Display a company No. field for billing addresses.</span>';
+$_['entry_company_no_required']  = 'Force Company No.:<br /><span class="help">Force customers to enter a their company no. for billing addresses.</span>';
+$_['entry_company_tax_display']  = 'Display Company No.:<br /><span class="help">Display a company No. field for billing addresses.</span>';
+$_['entry_company_tax_required'] = 'Force Company No.:<br /><span class="help">Force customers to enter a their company no. for billing addresses.</span>';
 $_['entry_account']           = 'Подтверждение при регистрации:<br /><span class="help">Требовать подтверждение согласия с правилами при регистрации аккаунта.</span>';
+$_['entry_cart_weight']       = 'Показывать вес на странице корзины:';
+$_['entry_guest_checkout']    = 'Гостевое оформление заказа:<br /><span class="help">Позволить клиентам оформлять заказы без регистрации Личного кабинета. Эта функция не будет доступна, если в корзине будут загружаемые в виде файла товары.</span>';
 $_['entry_checkout']          = 'Подтверждение при заказе:<br /><span class="help">Требовать подтверждение согласия с правилами при оформлении заказа.</span>';
 $_['entry_order_status']      = 'Статус заказа:<br /><span class="help">Статус заказа по умолчанию.</span>';
 $_['entry_stock_display']     = 'Показывать остаток на складе:<br /><span class="help">Показывать остаток товаров на складе на странице товара.</span>';
-$_['entry_stock_check']       = 'Показывать недостаток на складе:<br /><span class="help">Показывать сообщение о дефиците товара на складе на странице магазина.</span>';
 $_['entry_stock_checkout']    = 'Заказ при нехватке на складе:<br /><span class="help">Разрешить клиентам оформлять заказ, если заказываемых товаров на данный момент нет на складе.</span>';
 $_['entry_logo']              = 'Логотип:';
 $_['entry_icon']              = 'Иконка:';
+$_['entry_image_category']    = 'Размер изображения списка категорий:';
 $_['entry_image_thumb']       = 'Размер большого изображения товара:';
 $_['entry_image_popup']       = 'Размер всплывающего изображения товара:';
-$_['entry_image_category']    = 'Размер изображения списка категорий:';
 $_['entry_image_product']     = 'Размер изображения списка товаров:';
 $_['entry_image_additional']  = 'Размер дополнительных изображений товаров:';
 $_['entry_image_related']     = 'Размер изображения аналогичных товаров:';
+$_['entry_image_compare']       = 'Размер изображения списка сравнения:';
+$_['entry_image_wishlist']     = 'Размер изображения списка отложенных товаров:';
 $_['entry_image_cart']        = 'Размер изображений товаров в корзине:';
 $_['entry_image_manufacturer']  = 'Размер изображения списка производителей:';
-$_['entry_image_manufacturer']  = 'Размер изображения списка производителей:';
-$_['entry_image_compare']       = 'Размер изображения списка сравнения:';
-$_['entry_catalog_limit'] 	  = 'Элементов на страницу (Каталог):<br /><span class="help">Определяет количество элементов, показываемых на одной странице в каталоге (товары, категории и т.д.)</span>';
-$_['entry_cart_weight']       = 'Показывать вес на странице корзины:';
-
-// Button
-$_['button_add_store']        = 'Создать магазин';
-$_['button_delete_store']     = 'Удалить магазин';
+$_['entry_use_ssl']               = 'Использовать SSL:<br /><span class="help">Для использования SSL убедитесь что Ваш хостинг поддерживает SSL сертификат и пропишите SSL адреса в файле конфигурации.</span>';
 
 // Error
+$_['error_warning']            = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']        = 'У Вас нет прав для управления магазинами!';
 $_['error_name']              = 'Название магазина должно быть от 3 до 32 символов!';
+$_['error_owner']              = 'Store Owner must be between 3 and 64 characters!';
+$_['error_address']            = 'Store Address must be between 10 and 256 characters!';
+$_['error_email']              = 'E-Mail Address does not appear to be valid!';
+$_['error_telephone']          = 'Telephone must be between 3 and 32 characters!';
 $_['error_url']               = 'Необходим URL магазина!';
 $_['error_title']             = 'Заголовок магазина должен быть от 3 до 32 символов!';
+$_['error_limit']       	   = 'Не введены лимиты!';
 $_['error_image_thumb']       = 'Необходимо установить размер большого изображения товара!';
 $_['error_image_popup']       = 'Необходимо установить размер всплывающего изображения товара!';
-$_['error_image_category']    = 'Необходимо установить размер изображения списка категорий!';
 $_['error_image_product']     = 'Необходимо установить размер изображения списка товаров!';
+$_['error_image_category']    = 'Необходимо установить размер изображения списка категорий!';
+$_['error_image_manufacturer'] = 'Manufacturer List Size dimensions required!';
 $_['error_image_additional']  = 'Необходимо установить размер дополнительных изображений товаров!';
 $_['error_image_related']     = 'Необходимо установить размер изображения аналогичных товаров!';
+$_['error_image_compare']      = 'Compare Image Size dimensions required!';
+$_['error_image_wishlist']     = 'Wish List Image Size dimensions required!';
 $_['error_image_cart']        = 'Необходимо установить размер изображений товаров в корзине!';
+$_['error_default']            = 'Warning: You can not delete your default store!';
 $_['error_store']             = 'Этот магазин не может быть удален, поскольку он прикреплен к %s заказам!';
-$_['error_required_data']     = 'Не введены необходимые данные!';
-$_['error_limit']       	  = 'Не введены лимиты!';
 ?>
--- admin/language/russian/shipping/ups.php
+++ admin/language/russian/shipping/ups.php
@@ -64,16 +64,19 @@ $_['entry_city']                   = 'Origin City:<span class="help">Enter the n
 $_['entry_state']                  = 'Origin State/Province:<span class="help">Enter the two-letter code for your origin state/province.</span>';
 $_['entry_country']                = 'Origin Country:<span class="help">Enter the two-letter code for your origin country.</span>';
 $_['entry_postcode']               = 'Origin Zip/Postal Code:<span class="help">Enter your origin zip/postalcode.</span>';
-$_['entry_test']                   = 'Test or Production Mode:<span class="help">Use this module in Test or Production mode?</span>';
+$_['entry_test']                   = 'Test Mode:<span class="help">Use this module in Test (YES) or Production mode (NO)?</span>';
 $_['entry_quote_type']             = 'Quote Type:<span class="help">Quote for Residential or Commercial Delivery.</span>';
 $_['entry_service']                = 'Services:<span class="help">Select the UPS services to be offered.</span>';
+$_['entry_insurance']              = 'Enable Insurance:<span class="help">Enables insurance with product total as the value</span>';
 $_['entry_display_weight']         = 'Display Delivery Weight:<br /><span class="help">Do you want to display the shipping weight? (e.g. Delivery Weight : 2.7674 Kg\'s)</span>';
-$_['entry_weight_code']            = 'Weight Code:<br /><span class="help">Allowed kgs or lbs. Make sure you match the weight class with the UPS accepted weight code.</span>';
 $_['entry_weight_class']           = 'Weight Class:<span class="help">Set to kilograms or pounds.</span>';
-$_['entry_tax']                    = 'Tax Class:';
+$_['entry_length_class']           = 'Length Class:<span class="help">Set to centimeters or inches.</span>';
+$_['entry_dimension']			   = 'Dimensions (L x W x H):<br /><span class="help">This is assumed to be your average packing box size. Individual item dimensions are not supported at this time so you must enter average dimensions like 5x5x5.</span>';
+$_['entry_tax_class']              = 'Tax Class:';
 $_['entry_geo_zone']               = 'Geo Zone:';
 $_['entry_status']                 = 'Status:';
 $_['entry_sort_order']             = 'Sort Order:';
+$_['entry_debug']      			   = 'Debug Mode:<br /><span class="help">Saves send/recv data to the system log</span>';
 
 // Error
 $_['error_permission']             = 'Warning: You do not have permission to modify UPS (US) shipping!';
