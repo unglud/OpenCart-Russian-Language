19a162dd0c974606294db50e11be980ce30b8857
Alexander Matrosov <unglud@gmail.com>
2012-04-09 19:07:56 +0400

    #4 исправлена досаданя оплошность

--- catalog/language/russian/module/cart.php
+++ catalog/language/russian/module/cart.php
@@ -3,8 +3,8 @@
 $_['heading_title'] = 'В вашей корзине';
 
 // Text
-$['text_items'] = '%s шт. - %s';
-$['text_empty'] = 'Ваша корзина пуста!';
-$['text_cart'] = 'Открыть корзину';
-$['text_checkout'] = 'Оформить';
+$_['text_items'] = '%s шт. - %s';
+$_['text_empty'] = 'Ваша корзина пуста!';
+$_['text_cart'] = 'Открыть корзину';
+$_['text_checkout'] = 'Оформить';
 ?>
